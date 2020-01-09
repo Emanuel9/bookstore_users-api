cb7d671cbd40b8be5c7fd866cb11dd67da45d14e
Emanuel Dumitru <emanuelcristian.dumitru@crowdstrike.com>
2020-01-09 22:38:24 +0200

    Integration with mysql, add rows

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # bookstore_users-api
 Users API
+
+```
+CREATE SCHEMA `users_db` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish2_ci;
+```
+
+```
+CREATE TABLE `users_db`.`users` (
+    `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(45) NULL,
+    `last_name`  VARCHAR(45) NULL,
+    `email`     VARCHAR(45) NULL,
+    `data_created` VARCHAR(45) NULL,
+    PRIMARY KEY(`id`),
+    UNIQUE INDEX `email_UNIQUE` (`email` ASC));
+```
--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -2,8 +2,15 @@ package users
 
 import (
 	"fmt"
+	"github.com/Emanuel9/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/Emanuel9/bookstore_users-api/utils/date_utils"
 	"github.com/Emanuel9/bookstore_users-api/utils/errors"
+	"strings"
+)
+
+const(
+	indexUniqueEmail = "email_UNIQUE"
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, data_created) VALUES(?, ?, ?, ?); "
 )
 
 var (
@@ -11,6 +18,10 @@ var (
 )
 
 func (user *User) Get() *errors.RestError {
+	if err := users_db.Client.Ping(); err != nil {
+		panic(err)
+	}
+
 	result := usersDB[user.Id]
 	if result == nil {
 		return errors.NewNotFoundError(fmt.Sprintf("user %d not found", user.Id))
@@ -25,15 +36,38 @@ func (user *User) Get() *errors.RestError {
 }
 
 func (user *User) Save() *errors.RestError {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
+	}
+	defer stmt.Close()
+	user.DateCreated = date_utils.GetNowString()
+
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUniqueEmail) {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s is already taken", user.Email))
 		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user: %s", err.Error()))
 	}
 
-	user.DateCreated = date_utils.GetNowString()
-	usersDB[user.Id] = user
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when trying to save user: %s", err.Error()))
+	}
+	user.Id = userId
+	//
+	//current := usersDB[user.Id]
+	//if current != nil {
+	//	if current.Email == user.Email {
+	//		return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
+	//	}
+	//	return errors.NewBadRequestError(fmt.Sprintf("user %d already exists", user.Id))
+	//}
+	//
+	//user.DateCreated = date_utils.GetNowString()
+	//usersDB[user.Id] = user
 	return nil
 }
